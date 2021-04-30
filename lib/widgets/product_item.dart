7be3b0f450b06a5b2dd61a9d88f8d5a104fb872a
Raaj Kanchan <iamraajkanchan@gmail.com>
2021-04-30 23:19:31 +0530

    Updated on 30th April 2021

---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_app/screens/product_detail_screen.dart';
+
+class ProductItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final String imageURL;
+
+  ProductItem({this.id, this.title, this.imageURL});
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10.0),
+      child: GridTile(
+        child: GestureDetector(
+          onTap: () {
+            Navigator.of(context)
+                .pushNamed(ProductDetailScreen.routeName, arguments: id);
+          },
+          child: Image.network(
+            imageURL,
+            fit: BoxFit.cover,
+          ),
+        ),
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          title: Text(title, textAlign: TextAlign.center),
+          leading: IconButton(
+            icon: Icon(Icons.favorite),
+            onPressed: () {},
+            color: Theme.of(context).accentColor,
+          ),
+          trailing: IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () {},
+            color: Theme.of(context).accentColor,
+          ),
+        ),
+      ),
+    );
+  }
+}
